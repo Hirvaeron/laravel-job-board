facc7c47aed37da37840e3441a030738be73cd31
Hirvaeron <bodnar.alex07@gmail.com>
2025-08-22 18:36:09 +0200

    Jogosultsági policy-k implementálása.

--- app/Http/Controllers/MyJobController.php
+++ app/Http/Controllers/MyJobController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\JobRequest;
 use App\Models\Job;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Http\Request;
 
 class MyJobController extends Controller
 {
     public function index()
     {
+        Gate::authorize('viewAnyEmployer', Job::class);
         return view(
             'my_job.index',
             [
@@ -22,19 +24,16 @@ class MyJobController extends Controller
         );
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
+        Gate::authorize('create', Job::class);
         return view('my_job.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(JobRequest $request)
     {
+        Gate::authorize('create', Job::class);
         $request->user()->employer->jobs()->create($request->validated());
 
         return redirect()->route('my-jobs.index')->with('success', 'Job created successfully!');
@@ -42,22 +41,20 @@ class MyJobController extends Controller
 
     public function edit(Job $myJob)
     {
+        Gate::authorize('update', $myJob);
         return view('my_job.edit', ['job' => $myJob]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(JobRequest $request, Job $myJob)
     {
+        Gate::authorize('update', $myJob);
         $myJob->update($request->validated());
 
         return redirect()->route('my-jobs.index')->with('success', 'Job updated successfully!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(string $id)
     {
         //
